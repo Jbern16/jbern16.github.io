c034e7c9f4a864f0205b8d19853d33d6d8ea3436
Jbern16 <jbern16@gmail.com>
2016-05-03 23:04:15 -0400

    add type annotations

--- Jbern16.elm
+++ Jbern16.elm
@@ -3,7 +3,7 @@ module Jbern16 where
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
-import Array exposing (fromList, get )
+import Array exposing (fromList, get, Array )
 import StartApp.Simple as StartApp
 import Maybe exposing (withDefault)
 import String exposing (split)
@@ -16,6 +16,7 @@ type alias Model =
   , nextID : Int
   }
 
+init : Model
 init =
   { headline = "Jonathan Bernesser"
   , content = "Web Developer"
@@ -24,12 +25,15 @@ init =
 
 --UPDATE
 
+headlines : Array String 
 headlines =
   fromList [ "Jonathan Bernesser", "My Work:", "Contact Me:" ]
 
+contents : Array String
 contents =
   fromList [ "Web Developer", "https://github.com/jbern16", "mailto:jbern16@gmail.com, https://twitter.com/jbern16, https://medium.com/@jBern16, https://www.linkedin.com/in/jonathanbernesser" ]
 
+changeID : Model -> Int
 changeID model =
   if model.nextID == 2 then
      0
@@ -40,6 +44,7 @@ type Action
   = NoOp
   | NextClick
 
+update : Action -> Model -> Model
 update action model =
   case action of
     NoOp ->
@@ -55,14 +60,17 @@ update action model =
 
 --VIEW
 
+backgroundColors : Array String
 backgroundColors =
   fromList [ "#99D5C9", "#007EA7", "#5E8C61" ]
 
+backgroundStyle : String -> Html.Attribute
 backgroundStyle backgroundColor =
   style [ ( "backgroundColor", backgroundColor)
         , ( "height", "100vh")
         ]
 
+textContainer : Html.Attribute
 textContainer =
   style [ ( "paddingTop", "20%")
         , ( "paddingBottom", "20%")
@@ -70,24 +78,27 @@ textContainer =
         , ( "font-family", "Montserrat, sans-serif")
         ]
 
+iconStyle : Html.Attribute
 iconStyle =
   style [ ( "padding", "10px")
         , ( "color", "black" )
         ]
+
+sepStyle : Html.Attribute
 sepStyle =
   style [ ( "font-size", "28px" ) ]
 
+getLink : Array String -> Int -> String
 getLink content index =
   withDefault "" ( get index content )
 
-
+findContent : Model -> Html
 findContent model =
   if model.headline == "Contact Me:" then
     let
       links = fromList ( split ","  model.content )
       email    = getLink links 0
       twitter  = getLink links 1
-      medium   = getLink links 2
       linkedIn = getLink links 3
     in
       div  [ ]
@@ -95,8 +106,6 @@ findContent model =
           [ i [ class "fa fa-envelope fa-3x" ] [ ] ]
         , a [ iconStyle, href twitter  ]
           [ i [ class "fa fa-twitter fa-3x"  ] [ ] ]
-        , a [ iconStyle, href medium   ]
-          [ i [ class "fa fa-medium fa-3x"   ] [ ] ]
         , a [ iconStyle, href linkedIn ]
           [ i [ class "fa fa-linkedin fa-3x" ] [ ] ]
         ]
@@ -105,22 +114,28 @@ findContent model =
     let
       links = fromList ( split ","  model.content )
       github = getLink links 0
+      medium   = getLink links 2
     in
       div  [ ]
         [ a [ iconStyle, href github ]
           [ i [ class "fa fa-github fa-3x" ] [ ] ]
+        , a [ iconStyle, href medium   ]
+          [ i [ class "fa fa-medium fa-3x"   ] [ ] ]
         ]
   else
     div  [ style [ ("font-size", "24px") ] ] [ text model.content ]
 
+
+findSep : Model -> Html
 findSep model =
   if model.headline == "Contact Me:" then
-    span [ sepStyle ] [ text " ° "]
+    span [ sepStyle ] [ text " ° " ]
   else if model.headline == "My Work:" then
-    span [ sepStyle ] [ text " ° ° "]
+    span [ sepStyle ] [ text " ° ° " ]
   else
-    span [ sepStyle ] [ text " ° ° ° "]
+    span [ sepStyle ] [ text " ° ° ° " ]
 
+view : Signal.Address Action -> Model -> Html
 view address model =
   let
     backgroundColor = withDefault "" ( get (model.nextID ) backgroundColors)
@@ -135,6 +150,7 @@ view address model =
       ]
     ]
 
+main : Signal Html
 main =
   StartApp.start
     { model = init,
